1f797ef6d7b8a1623ca6e4a8de7f7cf3ed13bfbf
Kamil Wenta <gamerplrecording@gmail.com>
2021-01-14 23:01:59 +0100

    Format code

--- src/components/Changelog.js
+++ src/components/Changelog.js
@@ -1,5 +1,5 @@
 import React from "react";
-import changelog from "./Changelog.json";
+import changelog from "./changelog.json";
 
 import Button from "@material-ui/core/Button";
 import List from "@material-ui/core/List";
@@ -11,34 +11,30 @@ import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Divider from "@material-ui/core/Divider";
 
-const Release = ({ log }) => {
-    return (
-        <DialogContent>
-            <DialogContentText>{log.version}</DialogContentText>
-            <List>
-                {log.changes.map((change, index) => (
-                    <ListItem key={index}>{change}</ListItem>
-                ))}
-            </List>
-            <Divider />
-        </DialogContent>
-    );
-};
-
-const Changelog = props => {
-    return (
-        <Dialog open={props.open} onClose={props.handleClose} scroll="body" fullWidth maxWidth="md">
-            <DialogTitle id="changelog-title">Lista zmian</DialogTitle>
-
-            {changelog.map(log => (
-                <Release key={log.version} log={log} />
+const Release = ({ log }) => (
+    <DialogContent>
+        <DialogContentText>{log.version}</DialogContentText>
+        <List>
+            {log.changes.map((change, index) => (
+                <ListItem key={index}>{change}</ListItem>
             ))}
+        </List>
+        <Divider />
+    </DialogContent>
+);
 
-            <DialogActions>
-                <Button onClick={props.handleClose}>Zamknij</Button>
-            </DialogActions>
-        </Dialog>
-    );
-};
+const Changelog = props => (
+    <Dialog open={props.open} onClose={props.handleClose} scroll="body" fullWidth maxWidth="md">
+        <DialogTitle id="changelog-title">Lista zmian</DialogTitle>
+
+        {changelog.map(log => (
+            <Release key={log.version} log={log} />
+        ))}
+
+        <DialogActions>
+            <Button onClick={props.handleClose}>Zamknij</Button>
+        </DialogActions>
+    </Dialog>
+);
 
 export default Changelog;
